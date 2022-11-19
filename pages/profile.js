2faee1d53051b0bda80310e0ead24e66b2745d5a
Praditya <pradityaaldi.s@gmail.com>
2022-11-19 18:48:58 +0700

    change using next.js

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Profile() {
+  return (
+    <div>
+      <Head>
+        <title>Profile</title>
+      </Head>
+      <h1>Hello world</h1>
+    </div>
+  );
+}
