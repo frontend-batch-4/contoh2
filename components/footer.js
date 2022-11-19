b4fe2f6451eabce266109ea70b9e8cd73e0bbc40
Praditya <pradityaaldi.s@gmail.com>
2022-11-19 22:59:41 +0700

    update landing page
    note: belum selesai

---
+++ components/footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="social d-flex gap-4">
+              <div className="bg-primary">
+                <img src="/img/icon-instagram.svg" alt="icon instagram" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">Promosikan acaramu dan jual tiketmu dengan mudah</div>
+        </div>
+      </div>
+    </div>
+  );
+}
